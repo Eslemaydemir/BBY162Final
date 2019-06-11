013a7e7286480fe9ad713dd9010823e4f9b15ecc
Eslem Aydemir <33966124+Eslemaydemir@users.noreply.github.com>
2019-06-11 22:20:57 +0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,52 @@
+# author= Eslem Aydemir
+class islem:
+    print("Kütüphaneye hoşgeldin")
+    print("Kütüphanede bulunan kitaplar;")
+def katlistele(self):
+    listele= open("kitap.txt", "r+", encoding="utf-8")
+d = {}
+with open("kitap.txt") as f:
+    for line in f:
+        (key, val) = line.split(",")
+        d[str(key)] = val
+    print (d)
+
+def kayitekle(self):
+        ekle = open("kitap.txt", "a",)
+        b = input("Kayit giriniz.")
+        ekle.writelines(b)
+        for satirlar in ekle.readlines():
+            print(satirlar)
+        ekle.close()
+
+def eserarama(self):
+    ekayitara = open("kitap.txt", "r", encoding="utf-8")
+d = {}
+with open("kitap.txt") as f:
+    for line in f:
+        (key, val) = line.split(",")
+        d[str(key)] = val
+        a = str(val)
+    i = input("Yazarın Eserini Arayın:")
+    if i in d:
+        eyazdir= "{} yazdığı kitaplar: {}"
+        print(eyazdir.format(i,d[i]))
+    else:
+        print("Aradığınız kitap kütüphanemizde bulunmamaktadır.")
+
+def yazararama(self):
+    ykayitara = open("kitap.txt", "r", encoding="utf-8")
+dic = {}
+with open("eser.txt") as f:
+    for line in f:
+        (key, val) = line.split(",")
+        dic[str(key)] = val
+        a = str(val)
+    i = input("Kitabın yazarını arayın:")
+    if i in dic:
+        yyazdir= "{} kitabının yazarı: {}"
+        print(yyazdir.format(i,dic[i]))
+    else:
+        print("Aradığınız kitap kütüphanemizde bulunmamaktadır.")
+
+katalog = islem()
